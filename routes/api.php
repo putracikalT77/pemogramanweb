a5f867282d8e724fba628b08d92ad0e7609fc6f9
nadzif-yyy <nadzifzikri@gmail.com>
2026-06-26 06:16:24 +0700

    fix: keluarkan rute datatables dari middleware sanctum

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AdminAuthController::class, 'loginApi']);
 Route::post('/register', [AdminAuthController::class, 'register']);
+Route::get('/users-list', [UserController::class, 'getData'])->name('list-users');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('user', UserController::class);
-    Route::get('/users-list', [UserController::class, 'getData'])->name('list-users');
     Route::post('/logout', [AdminAuthController::class, 'logoutApi']);
 });
