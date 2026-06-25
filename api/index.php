06bcde4c79bdb1799d52f1678ae26a68e7470da3
nadzif-yyy <nadzifzikri@gmail.com>
2026-06-26 05:36:31 +0700

    fix: paksa vercel buka error dan gunakan direktori tmp

--- api/index.php
+++ api/index.php
@@ -1,2 +1,17 @@
 <?php
+$_SERVER['APP_DEBUG'] = 'true';
+$_ENV['APP_DEBUG'] = 'true';
+
+$_SERVER['APP_ENV'] = 'local';
+$_ENV['APP_ENV'] = 'local';
+
+$_SERVER['VIEW_COMPILED_PATH'] = '/tmp';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+
+$_SERVER['SESSION_DRIVER'] = 'cookie';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+
+$_SERVER['CACHE_STORE'] = 'array';
+$_ENV['CACHE_STORE'] = 'array';
+
 require __DIR__ . '/../public/index.php';
