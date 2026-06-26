8d1ed7215cefa83599562946c38f1dedb78fbcac
nadzif-yyy <nadzifzikri@gmail.com>
2026-06-26 21:31:42 +0700

    merubah footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,7 @@
     <!-- Footer -->
     <footer class="bg-dark text-white text-center py-3 mt-auto">
         <div class="container">
-            <p class="mb-0">&copy; 2026 jipjoki. All rights reserved.</p>
+            <p class="mb-0">&copy; 2026. All rights reserved.</p>
         </div>
     </footer>
 
